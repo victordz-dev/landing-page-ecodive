371803f667024da491a6f080db2aa9fe5ccf9b73
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2025-10-03 13:55:09 -0300

    fix: consertando os caminhos para login

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: 'src',
-    base: './',
+    base: '/landing-page-ecodive/',
 
     build: {
-        outDir: '../dist'
-    }
-})
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                login: resolve(__dirname, 'src/login.html'),
+            },
+        },
+    },
+});
